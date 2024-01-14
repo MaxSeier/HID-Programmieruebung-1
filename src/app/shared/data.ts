eef9681d7ca877af9062f4dfd2fdecb50249a573
MaxSeier <2210859001@fh-burgenland.at>
2024-01-14 12:18:04 +0100

    latest update: added pictures again

--- src/app/shared/data.ts
+++ src/app/shared/data.ts
@@ -1,100 +1,106 @@
 export var kindergardens = [
-    {
-        id: 0,
-        name: "Waldorfkindergarten im Zentrum Wiens",
-        address: "Oleandergasse 21, 1220 Wien", 
-        betreiber: "MA 10 - Wiener Kindergärten",
-        typ: "privat"
-    },
-    {
-        id: 1,
-        name: "Kindergarten der Karl Schubert Schule für seelenpflegebedürftige Kinder und Jugendliche in Wien",
-        address: "Kanitzgasse 1-3, 1230 Wien", 
-        betreiber: "Verein Karl Schubertschule für Seelenpflege- bedürftige Kinder und Jugendliche in Wien",
-        typ: "privat"
-    },
-    {
-        id: 2,
-        name: "Kindergarten der Stadt Wien",
-        address: "Audorfgasse 20, 1210 Wien", 
-        betreiber: "MA 10 - Wiener Kindergärten",
-        typ: "öffentlich"
-    },
-    {
-        id: 3,
-        name: "Kindergarten der Stadt Wien",
-        address: "Moßbachergasse 20-24, 1140 Wien", 
-        betreiber: "MA 10 - Wiener Kindergärten",
-        typ: "öffentlich"
-    },
-    {
-        id: 4,
-        name: "Montessori Kinderhaus Wien 1",
-        address: "Nußdorferstraße 6, 1090 Wien", 
-        betreiber: "Montessori Kinderhaus Wien",
-        typ: "privat"
-    }
+  {
+    id: 0,
+    name: "Waldorfkindergarten im Zentrum Wiens",
+    address: "Oleandergasse 21, 1220 Wien",
+    betreiber: "MA 10 - Wiener Kindergärten",
+    typ: "privat",
+    picturePath: "src/assets/images/Kindergarten_Logo_1.jpg",
+  },
+  {
+    id: 1,
+    name: "Kindergarten der Karl Schubert Schule für seelenpflegebedürftige Kinder und Jugendliche in Wien",
+    address: "Kanitzgasse 1-3, 1230 Wien",
+    betreiber: "Verein Karl Schubertschule für Seelenpflege- bedürftige Kinder und Jugendliche in Wien",
+    typ: "privat",
+    picturePath: "src/assets/images/Kindergarten_Logo_2.jpg",
+
+  },
+  {
+    id: 2,
+    name: "Kindergarten der Stadt Wien",
+    address: "Audorfgasse 20, 1210 Wien",
+    betreiber: "MA 10 - Wiener Kindergärten",
+    typ: "öffentlich",
+    picturePath: "src/assets/images/Kindergarten_Logo_3.jpg",
+  },
+  {
+    id: 3,
+    name: "Kindergarten der Stadt Wien",
+    address: "Moßbachergasse 20-24, 1140 Wien",
+    betreiber: "MA 10 - Wiener Kindergärten",
+    typ: "öffentlich",
+    picturePath: "src/assets/images/Kindergarten_Logo_4.jpg",
+  },
+  {
+    id: 4,
+    name: "Montessori Kinderhaus Wien 1",
+    address: "Nußdorferstraße 6, 1090 Wien",
+    betreiber: "Montessori Kinderhaus Wien",
+    typ: "privat",
+    picturePath: "src/assets/images/Kindergarten_Logo_5.jpg",
+  }
 ]
 
 export var children = [
-    {
-        id: '9b1deb4d-3b7d-4bad-9bpp-2b0d7b3dcb6d',
-        name: "Nicole Fitsch",
-        birthDate: "2021-07-04",
-        kindergarten: {
-            id: 0,
-            name: "Waldorfkindergarten im Zentrum Wiens",
-            address: "Oleandergasse 21, 1220 Wien", 
-            betreiber: "MA 10 - Wiener Kindergärten",
-            typ: "privat"
-        }
-    },
-    {
-        id: '9b1deb4d-897d-4bad-9bdd-2b0d7b3dcb6d',
-        name: "Jasmin Schmied",
-        birthDate: "2020-09-04",
-        kindergarten: {
-            id: 0,
-            name: "Waldorfkindergarten im Zentrum Wiens",
-            address: "Oleandergasse 21, 1220 Wien", 
-            betreiber: "MA 10 - Wiener Kindergärten",
-            typ: "privat"
-        },
-    },
-    {
-        id: '9b1deb4d-3b7d-4bad-9bdd-2b0d7b3dcb6d',
-        name: "Melanie Hollunder",
-        birthDate: "2020-07-04",
-        kindergarten: {
-            id: 1,
-            name: "Kindergarten der Karl Schubert Schule für seelenpflegebedürftige Kinder und Jugendliche in Wien",
-            address: "Kanitzgasse 1-3, 1230 Wien", 
-            betreiber: "Verein Karl Schubertschule für Seelenpflege- bedürftige Kinder und Jugendliche in Wien",
-            typ: "privat"
-        }
-    },
-    {
-        id: '1b9d6bcd-bbfd-4b2d-9b5d-ab8dfbbd4bed',
-        name: "Viktor Sachs",
-        birthDate: "2022-06-04",
-        kindergarten: {
-            id: 2,
-            name: "Kindergarten der Stadt Wien",
-            address: "Audorfgasse 20, 1210 Wien", 
-            betreiber: "MA 10 - Wiener Kindergärten",
-            typ: "öffentlich"
-        }
-    },
-    {
-        id: '9b1debop-3b7d-4bad-9bdd-2b0d7b3dcb6d',
-        name: "Emily Nilson",
-        birthDate: "2019-01-04",
-        kindergarten: {
-            id: 3,
-            name: "Kindergarten der Stadt Wien",
-            address: "Moßbachergasse 20-24, 1140 Wien", 
-            betreiber: "MA 10 - Wiener Kindergärten",
-            typ: "öffentlich"
-        }
+  {
+    id: '9b1deb4d-3b7d-4bad-9bpp-2b0d7b3dcb6d',
+    name: "Nicole Fitsch",
+    birthDate: "2021-07-04",
+    kindergarten: {
+      id: 0,
+      name: "Waldorfkindergarten im Zentrum Wiens",
+      address: "Oleandergasse 21, 1220 Wien",
+      betreiber: "MA 10 - Wiener Kindergärten",
+      typ: "privat"
     }
+  },
+  {
+    id: '9b1deb4d-897d-4bad-9bdd-2b0d7b3dcb6d',
+    name: "Jasmin Schmied",
+    birthDate: "2020-09-04",
+    kindergarten: {
+      id: 0,
+      name: "Waldorfkindergarten im Zentrum Wiens",
+      address: "Oleandergasse 21, 1220 Wien",
+      betreiber: "MA 10 - Wiener Kindergärten",
+      typ: "privat"
+    },
+  },
+  {
+    id: '9b1deb4d-3b7d-4bad-9bdd-2b0d7b3dcb6d',
+    name: "Melanie Hollunder",
+    birthDate: "2020-07-04",
+    kindergarten: {
+      id: 1,
+      name: "Kindergarten der Karl Schubert Schule für seelenpflegebedürftige Kinder und Jugendliche in Wien",
+      address: "Kanitzgasse 1-3, 1230 Wien",
+      betreiber: "Verein Karl Schubertschule für Seelenpflege- bedürftige Kinder und Jugendliche in Wien",
+      typ: "privat"
+    }
+  },
+  {
+    id: '1b9d6bcd-bbfd-4b2d-9b5d-ab8dfbbd4bed',
+    name: "Viktor Sachs",
+    birthDate: "2022-06-04",
+    kindergarten: {
+      id: 2,
+      name: "Kindergarten der Stadt Wien",
+      address: "Audorfgasse 20, 1210 Wien",
+      betreiber: "MA 10 - Wiener Kindergärten",
+      typ: "öffentlich"
+    }
+  },
+  {
+    id: '9b1debop-3b7d-4bad-9bdd-2b0d7b3dcb6d',
+    name: "Emily Nilson",
+    birthDate: "2019-01-04",
+    kindergarten: {
+      id: 3,
+      name: "Kindergarten der Stadt Wien",
+      address: "Moßbachergasse 20-24, 1140 Wien",
+      betreiber: "MA 10 - Wiener Kindergärten",
+      typ: "öffentlich"
+    }
+  }
 ]
